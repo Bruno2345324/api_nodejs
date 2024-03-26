a8352970f1bbb6bb7c741fc1052d269ca6030a55
Bruno_@20 <159494752+Bruno2345324@users.noreply.github.com>
2024-03-26 20:36:41 -0300

    Aula 26_03

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,67 +1,78 @@
-const { json } = require('express');
-const db = require('../database/connection');
+const { json } = require('express'); 
+const db = require('../database/connection'); 
 
 module.exports = {
-    async listarUsuarios(request, response) { //request é o que vem do frontend
-        try { //sistema try catch, vai passar pela primeira opção, se der erro em alguma linha ele vai entrar na segunda opção e mostrar a mensagem
+    async listarUsuarios(request, response) {
+        try {
+            // throw new Error('Eu causei o erro!');
+            // instruções SQL
+            const sql = `SELECT 
+                usu_id, usu_nome, usu_email, usu_dt_nasc, usu_senha, 
+                usu_tipo, usu_ativo = 1 AS usu_ativo 
+                FROM usuarios;`; 
+            // executa instruções SQL e armazena o resultado na variável usuários
+            const usuarios = await db.query(sql); 
+            const nItens = usuarios[0].length;
+
             return response.status(200).json({
-                sucesso: true,
-                mensagem: 'Lista de Usuários',
+                sucesso: true, 
+                mensagem: 'Lista de usuários.', 
+                dados: usuarios[0], 
+                nItens                 
+            });
+        } catch (error) {
+            // console.log(error);
+            return response.status(500).json({
+                sucesso: false, 
+                mensagem: 'Erro na requisição.', 
+                dados: error.message
+            });
+        }
+    },
+    async cadastrarUsuarios(request, response) {
+        try {
+            return response.status(200).json({
+                sucesso: true, 
+                mensagem: 'Cadastro de usuários.', 
                 dados: null
             });
         } catch (error) {
             return response.status(500).json({
-                sucesso: false,
-                mensagem: 'Erro na requisição \n ${error}',
+                sucesso: false, 
+                mensagem: `Erro na requisição. -${error}`, 
                 dados: null
             });
         }
-    },
-    async cadastrarUsuarios(request, response) { //request é o que vem do frontend
-        try { //sistema try catch, vai passar pela primeira opção, se der erro em alguma linha ele vai entrar na segunda opção e mostrar a mensagem
+    }, 
+    async editarUsuarios(request, response) {
+        try {
             return response.status(200).json({
-                sucesso: true,
-                mensagem: 'Cadastro de Usuários',
+                sucesso: true, 
+                mensagem: 'Editar usuários.', 
                 dados: null
             });
         } catch (error) {
             return response.status(500).json({
-                sucesso: false,
-                mensagem: 'Erro na requisição \n ${error}',
+                sucesso: false, 
+                mensagem: `Erro na requisição. -${error}`, 
                 dados: null
             });
         }
-    },
-    async editarUsuarios(request, response) { //request é o que vem do frontend
-        try { //sistema try catch, vai passar pela primeira opção, se der erro em alguma linha ele vai entrar na segunda opção e mostrar a mensagem
+    }, 
+    async apagarUsuarios(request, response) {
+        try {
             return response.status(200).json({
-                sucesso: true,
-                mensagem: 'Editar Usuários',
+                sucesso: true, 
+                mensagem: 'Apagar usuários.', 
                 dados: null
             });
         } catch (error) {
             return response.status(500).json({
-                sucesso: false,
-                mensagem: 'Erro na requisição \n ${error}',
+                sucesso: false, 
+                mensagem: `Erro na requisição. -${error}`, 
                 dados: null
             });
         }
-    },
-    
-    async apagarUsuarios(request, response) { //request é o que vem do frontend
-        try { //sistema try catch, vai passar pela primeira opção, se der erro em alguma linha ele vai entrar na segunda opção e mostrar a mensagem
-            return response.status(200).json({
-                sucesso: true,
-                mensagem: 'Apagar Usuários',
-                dados: null
-            });
-        } catch (error) {
-            return response.status(500).json({
-                sucesso: false,
-                mensagem: 'Erro na requisição \n ${error}',
-                dados: null
-            });
-        }
-    },
-    
+    }, 
 }
+
